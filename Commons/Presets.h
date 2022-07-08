d9ad2c79cce750c506782c7bd8c7fe76b16e744f
Котов Иван <kotov038@gmail.com>
2022-07-08 12:56:23 +0300

    refactoring

--- Commons/Presets.h
+++ Commons/Presets.h
@@ -25,18 +25,18 @@ struct IPERFParams {
     int ConnectionTime = 300;
 };
 
-#define LSHW_INSTALL "sudo apt-get install lshw -y"
+#define LSHW_INSTALL "apt-get install lshw -y"
 #define LSHW_INSTALL_OUT "lshw_install_info.txt"
-#define LSHW_TEST "sudo lshw -html -sanitize -numeric"
+#define LSHW_TEST " lshw -html -sanitize -numeric"
 #define LSHW_OUT "lshw_info.html"
 
 #define LSPCI_TEST "lspci -k | grep -i -EA2 net"
 #define LSPCI_OUT "lspci_info.txt"
 
-#define PING_TEST "sudo ping -c %i -s %i -i %.3g %s"
+#define PING_TEST "ping -c %i -s %i -i %.3g %s"
 #define PING_OUT "ping_info.txt"
 
-#define IPERF_INSTALL "sudo apt-get install iperf -y"
+#define IPERF_INSTALL "apt-get install iperf -y"
 #define IPERF_INSTALL_OUT "iperf_install_info.txt"
 #define IPERF_TEST "script -c 'iperf -c %s -P %i -r -t %i'"
 #define IPERF_OUT "ip_info.txt"
@@ -47,24 +47,24 @@ struct IPERFParams {
 
 #define LSPCI_VIDEO_TEST "lspci -v | grep -i VGA"
 #define LSPCI_VIDEO_TEST_3D "lspci -k | grep -EA2 'VGA|3D'"
-#define LSPCI_VIDEO_TEST_VIDEO_DRIVER "sudo lshw -c video | grep driver"
+#define LSPCI_VIDEO_TEST_VIDEO_DRIVER " lshw -c video | grep driver"
 #define LSPCI_VIDEO_OUT "vga_adapt-geter.txt"
 
 #define CPU_INFO_TEST "cat /proc/cpuinfo"
 #define CPU_INFO_OUT "base_cpu.txt"
 #define LSCPU_TEST "lscpu"
 #define LSCPU_OUT "info_lscpu.txt"
-#define CPU_FREQ_INSTALL "sudo apt-get install cpufrequtils -y"
+#define CPU_FREQ_INSTALL "apt-get install cpufrequtils -y"
 #define CPU_FREQ_INSTALL_OUT "cpu_install_info.txt"
 #define CPU_FREQ_TEST "сpufreq-info"
 #define CPU_FREQ_OUT "cpu_freq.txt"
 
-#define LM_SENSORS_INSTALL "sudo apt-get install lm-sensors -y"
+#define LM_SENSORS_INSTALL "apt-get install lm-sensors -y"
 #define LM_SENSORS_INSTALL_OUT "lm_sensors_install.txt"
 #define LM_SENSORS_TEST "sensors"
 #define LM_SENSORS_OUT "sensors_info.txt"
 
-#define VIDEO_STRESS_TEST_INSTALL "sudo apt-get install mesa-utils -y"
+#define VIDEO_STRESS_TEST_INSTALL "apt-get install mesa-utils -y"
 #define VIDE_STRESS_TEST_INSTALL_OUT "mesa_utils_install.txt"
 #define VIDEO_STRESS_TEST_INFO "glxgears -info"
 #define VIDEO_STRESS_TEST_INFO_OUT "glxgears_info.txt"
