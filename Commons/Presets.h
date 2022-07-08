eb36351753bf80be65501b6aeabb906c9992eacc
Котов Иван <kotov038@gmail.com>
2022-07-08 12:53:16 +0300

    refactoring

--- Commons/Presets.h
+++ Commons/Presets.h
@@ -25,7 +25,7 @@ struct IPERFParams {
     int ConnectionTime = 300;
 };
 
-#define LSHW_INSTALL "sudo apt install lshw -y"
+#define LSHW_INSTALL "sudo apt-get install lshw -y"
 #define LSHW_INSTALL_OUT "lshw_install_info.txt"
 #define LSHW_TEST "sudo lshw -html -sanitize -numeric"
 #define LSHW_OUT "lshw_info.html"
@@ -36,7 +36,7 @@ struct IPERFParams {
 #define PING_TEST "sudo ping -c %i -s %i -i %.3g %s"
 #define PING_OUT "ping_info.txt"
 
-#define IPERF_INSTALL "sudo apt install iperf -y"
+#define IPERF_INSTALL "sudo apt-get install iperf -y"
 #define IPERF_INSTALL_OUT "iperf_install_info.txt"
 #define IPERF_TEST "script -c 'iperf -c %s -P %i -r -t %i'"
 #define IPERF_OUT "ip_info.txt"
@@ -48,23 +48,23 @@ struct IPERFParams {
 #define LSPCI_VIDEO_TEST "lspci -v | grep -i VGA"
 #define LSPCI_VIDEO_TEST_3D "lspci -k | grep -EA2 'VGA|3D'"
 #define LSPCI_VIDEO_TEST_VIDEO_DRIVER "sudo lshw -c video | grep driver"
-#define LSPCI_VIDEO_OUT "vga_adapter.txt"
+#define LSPCI_VIDEO_OUT "vga_adapt-geter.txt"
 
 #define CPU_INFO_TEST "cat /proc/cpuinfo"
 #define CPU_INFO_OUT "base_cpu.txt"
 #define LSCPU_TEST "lscpu"
 #define LSCPU_OUT "info_lscpu.txt"
-#define CPU_FREQ_INSTALL "sudo apt install cpufrequtils -y"
+#define CPU_FREQ_INSTALL "sudo apt-get install cpufrequtils -y"
 #define CPU_FREQ_INSTALL_OUT "cpu_install_info.txt"
 #define CPU_FREQ_TEST "сpufreq-info"
 #define CPU_FREQ_OUT "cpu_freq.txt"
 
-#define LM_SENSORS_INSTALL "sudo apt install lm-sensors -y"
+#define LM_SENSORS_INSTALL "sudo apt-get install lm-sensors -y"
 #define LM_SENSORS_INSTALL_OUT "lm_sensors_install.txt"
 #define LM_SENSORS_TEST "sensors"
 #define LM_SENSORS_OUT "sensors_info.txt"
 
-#define VIDEO_STRESS_TEST_INSTALL "sudo apt install mesa-utils -y"
+#define VIDEO_STRESS_TEST_INSTALL "sudo apt-get install mesa-utils -y"
 #define VIDE_STRESS_TEST_INSTALL_OUT "mesa_utils_install.txt"
 #define VIDEO_STRESS_TEST_INFO "glxgears -info"
 #define VIDEO_STRESS_TEST_INFO_OUT "glxgears_info.txt"
